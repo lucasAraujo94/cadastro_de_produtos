1101a1c1bfcbc97e6819b795e9a9c15ad90539f0
lucasAraujo94 <98675611+lucasAraujo94@users.noreply.github.com>
2022-07-30 21:07:47 -0300

    Revert "Initial commit"
    This reverts commit e8d0e5466d3182eda5c5fac6247385b9751acb4f.

--- Cadastro_De_Produtos/src/Cadastro_De_Produtos.Identity/Data/CustomIdentity.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Cadastro_De_Produtos.Identity.Data
-{
-    public class CustomIdentity : IdentityUser
-    {
-        public string Nome { get; set; }
-    }
-}
